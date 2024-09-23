42937e6bfd37f61bb9a4c8279c1002d49a4d2c4f
Diego Ribeiro <102061960+Diego34ra@users.noreply.github.com>
2024-09-23 11:48:49 -0300

    23-09-2024 v1

--- src/main/java/br/edu/ifgoiano/ticket/controller/RegistroTrabalhoController.java
+++ src/main/java/br/edu/ifgoiano/ticket/controller/RegistroTrabalhoController.java
@@ -1,14 +1,24 @@
 package br.edu.ifgoiano.ticket.controller;
 
+import br.edu.ifgoiano.ticket.controller.dto.request.departamento.DepartamentoOutputDTO;
 import br.edu.ifgoiano.ticket.controller.dto.request.registroTrabalho.RegistroTrabalhoInputDTO;
 import br.edu.ifgoiano.ticket.controller.dto.request.registroTrabalho.RegistroTrabalhoInputUpdateDTO;
 import br.edu.ifgoiano.ticket.controller.dto.request.registroTrabalho.RegistroTrabalhoOutputDTO;
+import br.edu.ifgoiano.ticket.controller.exception.ErrorDetails;
 import br.edu.ifgoiano.ticket.service.RegistroTrabalhoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/registroTrabalho")
@@ -19,20 +29,53 @@ public class RegistroTrabalhoController {
     private RegistroTrabalhoService registroTrabalhoService;
 
     @PostMapping
+    @Operation(summary = "Criar um registro")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Registro criado com sucesso.",content = { @Content(mediaType = "application/json", schema = @Schema(implementation = RegistroTrabalhoOutputDTO.class))}),
+            @ApiResponse(responseCode = "401", description = "Acesso negado.",content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))})
+    })
     public ResponseEntity<RegistroTrabalhoOutputDTO> criar(@RequestParam Long ticketId, @RequestBody RegistroTrabalhoInputDTO registroTrabalhoInputDTO){
         var registroCriado = registroTrabalhoService.criar(ticketId,registroTrabalhoInputDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(registroCriado);
     }
 
+    @GetMapping
+    @Operation(summary = "Buscar todos os registros de um ticket")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Registros buscados com sucesso.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = RegistroTrabalhoOutputDTO.class))
+                    )
+            ),
+            @ApiResponse(responseCode = "401", description = "Acesso negado.",content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))})
+    })
+    public ResponseEntity<List<RegistroTrabalhoOutputDTO>> buscarTodos(@RequestParam Long ticketId){
+        var registroList = registroTrabalhoService.buscarTodosPorTicket(ticketId);
+        return ResponseEntity.status(HttpStatus.OK).body(registroList);
+    }
+
     @PutMapping("{id}")
-    public ResponseEntity<RegistroTrabalhoOutputDTO> atualizar(@RequestParam Long registroId, @RequestBody RegistroTrabalhoInputUpdateDTO registroTrabalhoInputUpdateDTO){
-        var registroAtualizado = registroTrabalhoService.atualizar(registroId,registroTrabalhoInputUpdateDTO);
+    @Operation(summary = "Atualizar um registro")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Registro atualizado com sucesso.",content = { @Content(mediaType = "application/json", schema = @Schema(implementation = RegistroTrabalhoOutputDTO.class))}),
+            @ApiResponse(responseCode = "401", description = "Acesso negado.",content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))})
+    })
+    public ResponseEntity<RegistroTrabalhoOutputDTO> atualizar(@RequestParam Long id, @RequestBody RegistroTrabalhoInputUpdateDTO registroTrabalhoInputUpdateDTO){
+        var registroAtualizado = registroTrabalhoService.atualizar(id,registroTrabalhoInputUpdateDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(registroAtualizado);
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<?> deletarPorId(@RequestParam Long registroId){
-        registroTrabalhoService.deletarPorId(registroId);
+    @Operation(summary = "Deletar um registro")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Registro deletado com sucesso."),
+            @ApiResponse(responseCode = "401", description = "Acesso negado.",content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))})
+    })
+    public ResponseEntity<?> deletarPorId(@RequestParam Long id){
+        registroTrabalhoService.deletarPorId(id);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
--- src/main/java/br/edu/ifgoiano/ticket/controller/RegraPrioridadeController.java
+++ src/main/java/br/edu/ifgoiano/ticket/controller/RegraPrioridadeController.java
@@ -1,8 +1,17 @@
 package br.edu.ifgoiano.ticket.controller;
 
+import br.edu.ifgoiano.ticket.controller.dto.request.registroTrabalho.RegistroTrabalhoOutputDTO;
 import br.edu.ifgoiano.ticket.controller.dto.request.regraPrioridade.RegraPrioridadeInputDTO;
 import br.edu.ifgoiano.ticket.controller.dto.request.regraPrioridade.RegraPrioridadeOutputDTO;
+import br.edu.ifgoiano.ticket.controller.exception.ErrorDetails;
 import br.edu.ifgoiano.ticket.service.RegraPrioridadeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +22,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/regraPrioridades")
+@Tag(name = "Regra Prioridade")
 public class RegraPrioridadeController {
 
 
@@ -20,24 +30,51 @@ public class RegraPrioridadeController {
     private RegraPrioridadeService regraPrioridadeService;
 
     @PostMapping
+    @Operation(summary = "Criar um regra de prioridade")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Regra criada com sucesso.",content = { @Content(mediaType = "application/json", schema = @Schema(implementation = RegraPrioridadeOutputDTO.class))}),
+            @ApiResponse(responseCode = "401", description = "Acesso negado.",content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))})
+    })
     public ResponseEntity<RegraPrioridadeOutputDTO> criar(@RequestBody RegraPrioridadeInputDTO regraPrioridadeInputDTO){
         var regraPrioridadeCriada = regraPrioridadeService.criar(regraPrioridadeInputDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(regraPrioridadeCriada);
     }
 
     @GetMapping
+    @Operation(summary = "Buscar todas as regras de prioridade")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Regras buscadas com sucesso.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = RegraPrioridadeOutputDTO.class))
+                    )
+            ),
+            @ApiResponse(responseCode = "401", description = "Acesso negado.",content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))})
+    })
     public ResponseEntity<List<RegraPrioridadeOutputDTO>> buscarTodos(){
         return ResponseEntity.status(HttpStatus.OK).body(regraPrioridadeService.buscarTodos());
     }
 
     @PutMapping("{id}")
+    @Operation(summary = "Atualizar uma regra de prioridade")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Regra atualizada com sucesso.",content = { @Content(mediaType = "application/json", schema = @Schema(implementation = RegraPrioridadeOutputDTO.class))}),
+            @ApiResponse(responseCode = "401", description = "Acesso negado.",content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))})
+    })
     public ResponseEntity<RegraPrioridadeOutputDTO> atualizar(@PathVariable Long id, @RequestBody RegraPrioridadeInputDTO regraPrioridadeInputDTO){
         var regraPrioridadeAtualizada = regraPrioridadeService.atualizar(id,regraPrioridadeInputDTO);
         return ResponseEntity.status(HttpStatus.OK).body(regraPrioridadeAtualizada);
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<?> atualizar(@PathVariable Long id){
+    @Operation(summary = "Deletar uma regra de prioridade")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Regra deletada com sucesso."),
+            @ApiResponse(responseCode = "401", description = "Acesso negado.",content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))})
+    })
+    public ResponseEntity<?> deletarPorId(@PathVariable Long id){
         regraPrioridadeService.deletarPorId(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
