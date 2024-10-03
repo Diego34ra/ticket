115094849b2800bb7f3d4d2421ac3b3866aac69c
Diego Ribeiro <102061960+Diego34ra@users.noreply.github.com>
2024-10-02 23:49:17 -0300

    02-10-2024 v2

--- src/main/java/br/edu/ifgoiano/ticket/producer/TicketProducer.java
+++ src/main/java/br/edu/ifgoiano/ticket/producer/TicketProducer.java
@@ -1,7 +1,26 @@
 package br.edu.ifgoiano.ticket.producer;
 
+import br.edu.ifgoiano.ticket.controller.dto.request.EmailDTO;
+import br.edu.ifgoiano.ticket.model.Usuario;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TicketProducer {
+
+    final RabbitTemplate rabbitTemplate;
+
+    @Value("${broker.queue.email.name}")
+    private String routingKey;
+
+    public TicketProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void publishMessageEmail(EmailDTO emailDTO){
+        System.out.println(routingKey);
+        rabbitTemplate.convertAndSend("",routingKey,emailDTO);
+    }
 }
